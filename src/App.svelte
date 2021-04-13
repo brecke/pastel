0287838933c25d928678fa0ca502e1d8c4715159
Miguel Laginha <me@miguellaginha.com>
2021-04-13 16:13:39 +0100

    feat: router cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Router, Link, Route } from "svelte-navigator";
+  import { Router, Route } from "svelte-navigator";
   import Homepage from "./routes/Homepage.svelte";
   import Dashboard from "./routes/Dashboard.svelte";
   import Library from "./routes/Library.svelte";
@@ -7,32 +7,8 @@
 </script>
 
 <Router>
-  <!-- <nav>
-    <Link to="/">Homepage</Link>
-    <Link to="dashboard">Dashboard</Link>
-    <Link to="library">Library</Link>
-    <Link to="groups">Groups</Link>
-  </nav>
--->
-  <div>
-    <Route path="/">
-      <Homepage />
-    </Route>
-    <Route path="dashboard" component={Dashboard} />
-    <Route path="library" component={Library} />
-    <Route path="groups" component={Groups} />
-    <!--
-      <Route path="blog/*">
-        <Route path="/">
-          <Blog />
-        </Route>
-        <Route path=":id" component={BlogPost} />
-      </Route>
-    -->
-    <!--  
-  <Route path="search/:query" let:params>
-    <Search query={params.query} />
-  </Route>
-   -->
-  </div>
+  <Route useFocus path="/" component={Homepage} />
+  <Route path="dashboard" component={Dashboard} />
+  <Route path="library" component={Library} />
+  <Route path="groups" component={Groups} />
 </Router>
