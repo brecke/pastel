d7ad84699f56f866022c5e9ffe72b99f945052c0
Miguel Laginha <me@miguellaginha.com>
2021-04-13 16:14:31 +0100

    refactor: made redirectUrl into a writable store

--- src/components/ExternalAuthStrategy.svelte
+++ src/components/ExternalAuthStrategy.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  import {
-    getLoginRedirectUrl as getRedirectUrl,
-    getInvitationInfo,
-  } from "../helpers/utils";
   import anylogger from "anylogger";
   import { onMount } from "svelte";
+  import { redirectUrl, invitationInfo } from "../stores/user";
 
   const log = anylogger("external-auth-strategy");
 
@@ -12,33 +9,29 @@
   export let id: string;
   export let url: string;
   export let icon: string;
-  let redirectUrl: string;
   let invitationToken: string;
   let invitationEmail: string;
 
   onMount(async () => {
-    redirectUrl = getRedirectUrl();
-
     // Variable that keeps track of the invitation info that is available in the page context, if any
-    const invitationInfo = getInvitationInfo();
-    invitationToken = invitationInfo.token;
-    invitationEmail = invitationInfo.email;
+    invitationToken = $invitationInfo.token;
+    invitationEmail = $invitationInfo.email;
 
     // TODO debug
-    log.debug(`redirectUrl: ${redirectUrl}`);
-    log.debug(`invitation info: ${invitationEmail} / ${invitationToken}`);
+    log.warn(`redirectUrl: ${redirectUrl}`);
+    log.warn(`invitation info: ${invitationEmail} / ${invitationToken}`);
   });
 
   const submitForm = (e: Event) => {
     // TODO
-    log.debug("Here I go, using external auth again...");
+    log.warn("Here I go, using external auth again...");
   };
 </script>
 
 <form on:submit|preventDefault={submitForm} action={url} method="POST">
   <input type="hidden" name="invitationToken" bind:value={invitationToken} />
   <input type="hidden" name="invitationEmail" bind:value={invitationEmail} />
-  <input type="hidden" name="redirectUrl" bind:value={redirectUrl} />
+  <input type="hidden" name="redirectUrl" bind:value={$redirectUrl} />
   <a
     on:click={submitForm}
     href="/"
